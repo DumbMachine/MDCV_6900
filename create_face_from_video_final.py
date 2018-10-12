33d4a731cccfc8ffb27c1351e7d2922d16d0e531
Ratin_Kumar <ratin.kumar.2k@gmail.com>
2018-10-12 23:01:30 +0530

    Changed cv2 to imutils for video streaming.

--- create_face_from_video_final.py
+++ create_face_from_video_final.py
@@ -1,11 +1,16 @@
 import numpy as np
 import cv2
+from imutils.video import FileVideoStream
+import sys
+import imutils
+from imutils.video import FPS,FileVideoStream
 import pandas as pd
 from PIL import Image
+from scipy import ndimage
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 def detect(gray, frame):
     roi_gray=0
-    faces = face_cascade.detectMultiScale(gray, 1.0, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
@@ -14,11 +19,16 @@ def detect(gray, frame):
         #cv2.imshow("rando",roi_color)
     return frame,roi_gray
 lis_img=[]
+frames=0
 cap = cv2.VideoCapture(r"C:\Users\ratin\Downloads\videoplayback.mp4")
-
+fps = FPS().start()
+cap.set(3,100)
+cap.set(4,50)
 while(cap.isOpened()):
     ret, frame = cap.read()
+    frame = imutils.resize(frame, width=450)
     if ret==True:
+        frames+=1
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
         frame,roi_gray = detect(gray,gray)
@@ -33,9 +43,12 @@ while(cap.isOpened()):
     else:
         cap.release()
         cv2.destroyAllWindows()
-    
+fps.stop()
+print("[INFO] fps is: {}".format(frames/fps.elapsed()))
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 cap.release()
 cv2.destroyAllWindows()
+
 lis=[0 for _ in range(len(lis_img))]
 for i in range(len(lis_img)):
     lis[i]=lis_img[i].ravel()
@@ -58,14 +71,21 @@ x = pd.DataFrame(data)
 x.to_csv("fromvideotrial.csv")
 
 def create_face_from_video(filename):
+    frames=0
     lis_img=[]
-    cap = cv2.VideoCapture(filename)
+    fvs = FileVideoStream(filename).start()
+    time.sleep(1.0)
+    fps = FPS().start()
     
-    while(cap.isOpened()):
-        ret, frame = cap.read()
+    while fvs.more():
+        frame = fvs.read()
+        frame = imutils.resize(frame, width=450)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = np.dstack([frame, frame, frame])
         if ret==True:
+            frames+=1
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = ndimage.rotate(gray,90)
+            #gray = ndimage.rotate(gray,90)
             frame,roi_gray = detect(gray,gray)
             if type(roi_gray)!=int:
                     roi_gray = Image.fromarray(roi_gray)
@@ -73,14 +93,17 @@ def create_face_from_video(filename):
                     roi_gray = np.array(roi_gray)
                     lis_img.append(roi_gray)
             cv2.imshow('frame',frame)
+            fps.update()
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
             cap.release()
             cv2.destroyAllWindows()
         
+    fps.stop()
     cap.release()
     cv2.destroyAllWindows()
+    print("hehe: ",frames/fps.elapsed())
     lis=[0 for _ in range(len(lis_img))]
     for i in range(len(lis_img)):
         lis[i]=lis_img[i].ravel()
@@ -89,7 +112,7 @@ def create_face_from_video(filename):
     #initsializeasd
     data["label"]=[]
     for i in range(len(lis)):
-        data["label"].append(11)
+        data["label"].append(69)
     for i in range(len(lis)):
         for j in range(784):#len(lis[0])
             data["pixel{}" .format(j+1)]=[]
@@ -100,5 +123,7 @@ def create_face_from_video(filename):
     
         
     x = pd.DataFrame(data)
-    x.to_csv("guju.csv")
+    x.to_csv("shibu.csv")
+
+create_face_from_video("WIN_20181012_13_27_57_Pro.mp4")
     
